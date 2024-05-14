efb847562d69d001ece1e32ba27acd5cd6e52904
Satish Kumar Sonker <btech.csit@gmail.com>
2024-05-14 11:35:53 +0530

    Add Excel Reader and Writer

---
+++ Services/IExcelReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxSolutions.Reclaim.ExcelReader
+{
+    public interface IExcelReader
+    {
+        DataTable ReadExcelAsDataTable(string excelFilePath, string tabName, int skipRows = 0);
+        char[] RemoveCharFromColName { get; set; }
+    }
+}
---
+++ Services/IExcelWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxSolutions.Reclaim.ExcelWriter
+{
+    public interface IExcelWriter
+    {
+        string ExcelFromDataTable(DataTable dataTable, string filePath, string fileName, string tabName);
+    }
+}
